632a163dc2196b9285a2805a90112936954ed37f
Anant <91617698+p-anant@users.noreply.github.com>
2022-01-09 01:53:50 +0530

    Update ProductOfArray.py

--- Arrays/ProductOfArray.py
+++ Arrays/ProductOfArray.py
@@ -1,3 +1,8 @@
+# Problem Statement
+# You have been given an integer array/list (ARR) of size N. You have to return an array/list PRODUCT such that PRODUCT[i] 
+# is equal to the product of all the elements of ARR except ARR[i]
+#  Note : Each product can cross the integer limits, so we should take modulo of the operation. 
+# Take MOD = 10^9 + 7 to always stay in the limits.
 
 from sys import stdin
 
